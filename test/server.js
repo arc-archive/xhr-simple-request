de7683418f8ca6bb317a732db677437d6b14bba7
Pawel Psztyc <jarrodek@gmail.com>
2018-03-31 17:22:16 -0700

    Initial commit

---
+++ test/server.js
@@ -0,0 +1,104 @@
+/* global sinon */
+
+/**
+ * Mocking server for tests
+ */
+class MockServer {
+  /**
+   * Creates server endpoints
+   */
+  createServer() {
+    this.srv = sinon.fakeServer.create({
+      autoRespond: true
+    });
+    this.mock();
+  }
+  /**
+   * Restores XHR object.
+   */
+  restore() {
+    this.srv.restore();
+  }
+  /**
+   * Creates endpoints
+   */
+  mock() {
+    this.mockSuccess();
+    this.mockSuccessJson();
+    this.mockSuccessXml();
+    this.mockSuccessPost();
+    this.mockSuccessHeaders();
+    this.mock404();
+  }
+  /**
+   * Mocks success response
+   */
+  mockSuccess() {
+    const url = 'http://success.domain.com/';
+    this.srv.respondWith('GET', url, (xhr) => {
+      xhr.respond(200, {
+        'Content-Type': 'text/plain'
+      }, 'test');
+    });
+  }
+  /**
+   * Mocks success response for JSON
+   */
+  mockSuccessJson() {
+    const url = 'http://success.domain.com/json';
+    this.srv.respondWith('GET', url, (xhr) => {
+      xhr.respond(200, {
+        'Content-Type': 'application/json'
+      }, JSON.stringify({'test': true}));
+    });
+  }
+
+  /**
+   * Mocks success response for XML
+   */
+  mockSuccessXml() {
+    const url = 'http://success.domain.com/xml';
+    this.srv.respondWith('GET', url, (xhr) => {
+      xhr.respond(200, {
+        'Content-Type': 'application/xml'
+      }, '<test></test>');
+    });
+  }
+
+  /**
+   * Mocks success response for POST method
+   */
+  mockSuccessPost() {
+    const url = 'http://success.domain.com/post';
+    this.srv.respondWith('POST', url, (xhr) => {
+      xhr.respond(200, {
+        'Content-Type': 'text/plain'
+      }, xhr.requestBody);
+    });
+  }
+
+  /**
+   * Mocks success response to test headers
+   */
+  mockSuccessHeaders() {
+    const url = 'http://success.domain.com/headers';
+    this.srv.respondWith('GET', url, (xhr) => {
+      xhr.respond(200, {
+        'Content-Type': 'text/plain'
+      }, JSON.stringify(xhr.requestHeaders));
+    });
+  }
+
+  /**
+   * Mocks 404 error response
+   */
+  mock404() {
+    const url = 'http://error.domain.com/404';
+    this.srv.respondWith('GET', url, (xhr) => {
+      xhr.respond(404, {
+        'Content-Type': 'text/plain'
+      }, 'e404');
+    });
+  }
+}
+window.mockServer = new MockServer();
